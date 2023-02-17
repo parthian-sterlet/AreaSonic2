7c7315a0203f7399b7f56c8e799955861ecce90f
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-02-17 13:37:35 +0700

    Add files via upload

---
+++ src/1.bat
@@ -0,0 +1,1 @@
+C:\Strawberry\perl\bin\perl.exe test_bed.pl EIN3_Chang DHS_Ath_seedling_normal DHS_Ath_root_normal at 1000 asb.txt asb.dist
---
+++ src/2.bat
@@ -0,0 +1,1 @@
+C:\Strawberry\perl\bin\perl.exe test_nobed.pl EIN3_Chang ein3_deg ups1500_protcod_tair10 at 1000 as.txt as.dist
