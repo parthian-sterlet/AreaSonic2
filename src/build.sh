1e31d92bcff73481f39f2d3af3803fafb5e4b1d6
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-02-17 13:50:40 +0700

    Create build.sh

---
+++ src/build.sh
@@ -0,0 +1,6 @@
+g++ -o area_shuffling_pair area_shuffling_pair.cpp
+g++ -o area_shuffling_pair_bed area_shuffling_pair_bed.cpp
+g++ -o tabnslolbik_wordget tabnslolbik_wordget.cpp
+chmod a+x area_shuffling_pair_bed
+chmod a+x area_shuffling_pair
+chmod a+x tabnslolbik_wordget
